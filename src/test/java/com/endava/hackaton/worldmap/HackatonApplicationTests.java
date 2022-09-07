9a6e8fefdd97eea482c939e09e1555c3045969a3
Biancazel <biankazel@yahoo.com>
2022-09-07 11:37:19 +0300

    change java version to 17

---
+++ src/test/java/com/endava/hackaton/worldmap/HackatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.hackaton.worldmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
